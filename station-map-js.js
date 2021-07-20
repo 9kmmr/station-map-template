b0f1a3bf252161dd24c85b466d45dfad0924ca4e
9kmmr <tunglam195@gmail.com>
2021-07-20 12:27:27 +0700

    new style

--- station-map-js.js
+++ station-map-js.js
@@ -1,61 +1,79 @@
 (function ($) {
+  const map = L.map("map_station_map").setView([38.013, 142.251], 5);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map);
 
+  var mcIcon = L.Icon.extend({
+    options: {
+      iconUrl: map_station_url + "MCpin.png",
+      iconSize: [32, 52],
+      iconAnchor: [16, 52],
+      popupAnchor: [-3, -55],
+    },
+  });
 
-    function ajax_get_map_stations(callback) {
-        $.ajax({
-            type: "GET",
-            url: "/wp-admin/admin-ajax.php?order=" + map_station_order + "&orderby=" + map_station_orderby,
-            data: {
-                action: 'get_station_posts'
-            },
+  function ajax_get_map_stations(callback) {
+    $.ajax({
+      type: "GET",
+      url:
+        "/wp-admin/admin-ajax.php?order=" +
+        map_station_order +
+        "&orderby=" +
+        map_station_orderby,
+      data: {
+        action: "get_station_posts",
+      },
 
-            success: function (response) {
-
-                callback(response)
-            }
-        });
-    }
-
-
-
-
-    $(function () {
-        if ($('#map_station_map').length) {
-            ajax_get_map_stations(function (response) {
-
-                response = JSON.parse(response);
-
-                if (response.length) {
-
-                    var map = L.map('map_station_map').setView([38.013, 142.251], 5);
-                    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors' }).addTo(map);
-
-                    var mcIcon = L.Icon.extend({ options: { iconUrl: map_station_url + 'MCpin.png', iconSize: [32, 52], iconAnchor: [16, 52], popupAnchor: [-3, -55] } });
-
-                    for (let index = 0; index < response.length; index++) {
-                        const element = response[index];
-                        console.log(element)
-                        L.marker(
-                            [element?.location?.lat, element?.location?.lng],
-                            { icon: new mcIcon() }).bindPopup(
-                                `<b>${element['title']}</b>
-                                <br />${element['information']}<br />
-                                Tel: ${element['telephone']}<br />
-                                <a href='${element['homepage']}' target='_blank'>${element['homepage'].replace(/(^\w+:|^)\/\//, '')}</a><br />
-                                <a href='https://maps.google.de/maps?q=${element['location']['lat']},${element['location']['lng']}' target='_blank'>Show in Google Maps</a>
-                            `).addTo(map);
-
-                    }
-                }
-
-            });
-
-
-
-
-
-        }
+      success: function (response) {
+        callback(response);
+      },
     });
+  }
 
+  $(function () {
+    $(".map_station_mini_map").each(function (index) {
+      let map_station_sec = $(this).parent().parent();
 
-})(jQuery)
+      let mini_map = L.map($(this).attr("id"), { zoomControl: false });
+      L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        attribution: "",
+      }).addTo(mini_map);
+      mini_map.setView(
+        [$(map_station_sec).data("lat"), $(map_station_sec).data("lng")],
+        15
+      );
+      L.marker(
+        [$(map_station_sec).data("lat"), $(map_station_sec).data("lng")],
+        {
+          icon: new mcIcon(),
+        }
+      ).addTo(mini_map);
+
+      L.marker(
+        [$(map_station_sec).data("lat"), $(map_station_sec).data("lng")],
+        {
+          icon: new mcIcon(),
+        }
+      )
+        .bindPopup(
+          `<b>${$(map_station_sec).data("title")}</b>
+                          <br />${$(map_station_sec).data("information")}<br />
+                          Tel: ${$(map_station_sec).data("telephone")}<br />
+                          <a href='${$(map_station_sec).data(
+                            "homepage"
+                          )}' target='_blank'>${$(map_station_sec)
+            .data("homepage")
+            .replace(/(^\w+:|^)\/\//, "")}</a><br />
+                          <a href='https://maps.google.de/maps?q=${$(
+                            map_station_sec
+                          ).data("lat")},${$(map_station_sec).data(
+            "lng"
+          )}' target='_blank'>Show in Google Maps</a>
+                      `
+        )
+        .addTo(map);
+    });
+  });
+})(jQuery);
--- station-map-template-page.php
+++ station-map-template-page.php
@@ -11,29 +11,70 @@ $favicon = get_site_icon_url();
 
 if (post_type_exists('station')) {
 
-    $all_station_args = [
+    /* $all_station_args = [
         'numberposts' => $atts['num'],
         'order' => $atts['order'],
         'orderby' => $atts['orderby'],
-        'post_type' => 'station'
+        'post_type' => 'station',
+        'suppress_filters' => 0
 
     ];
-    $all_station = get_posts($all_station_args);
+    $all_station = get_posts($all_station_args); */
 
-    if (function_exists('get_field')) {
+    $cargs = array(
+        'child_of'      => 0,
+        'orderby'       => 'name',
+        'order'         => 'ASC',
+        'hide_empty'    => 1,
+        'taxonomy'      => 'region', //change this to any taxonomy,
+        'suppress_filters' => 0, 
+    );
 
-        $all_station = array_map(function ($post) use ($favicon) {
+    $datas = [];
 
-            return [
-                "thumbnail" =>  has_post_thumbnail($post->ID) ? wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'thumbnail') : $favicon,
-                "title" => $post->post_title,
-                "location" => get_field('location', $post->ID),
-                "information" =>  get_field('information', $post->ID, false, false),
-                "homepage" =>  get_field('homepage', $post->ID),
-                "telephone" =>  get_field('telephone', $post->ID),
-                "email" =>  get_field('email', $post->ID),
+    foreach (get_categories($cargs) as $tax) {
+        // List posts by the terms for a custom taxonomy of any post type   
+        $args = array(
+            'post_type'         => 'station',
+            'post_status'       => 'publish',
+            'posts_per_page'    => $atts['num'],
+            'order'             => $atts['order'],
+            'orderby'           => $atts['orderby'],   
+            'suppress_filters' => 0,         
+            'tax_query' => array(
+                array(
+                    'taxonomy'  => 'region',
+                    'field'     => 'slug',
+                    'terms'     => $tax->slug
+                )
+            )
+        );
+
+        $all_station = get_posts($args);
+        if (function_exists('get_field')) {
+    
+            $all_station = array_map(function ($post) use ($favicon) {
+    
+                return [
+                    "id" => $post->ID,
+                    "thumbnail" =>  has_post_thumbnail($post->ID) ? wp_get_attachment_url(get_post_thumbnail_id($post->ID), 'thumbnail') : $favicon,
+                    "title" => $post->post_title,
+                    "location" => get_field('location', $post->ID),
+                    "information" =>  get_field('information', $post->ID, false, false),
+                    "homepage" =>  get_field('homepage', $post->ID),
+                    "telephone" =>  get_field('telephone', $post->ID),
+                    "email" =>  get_field('email', $post->ID),
+                ];
+            }, $all_station);
+
+            $datas[] = [
+                "region" => $tax->name,
+                "region-slug" => $tax->slug,
+                "stations" => $all_station
             ];
-        }, $all_station);
+        }
+
+
     }
 
 ?>
@@ -47,30 +88,45 @@ if (post_type_exists('station')) {
 
         </div>
         <div class="map_station_main_content">
-            <?php if (count($all_station)) {
-                foreach ($all_station as $key => $station) {
-
+            <?php if (count($datas)) {
+                foreach ($datas as $key => $data) {
 
             ?>
-                    <div class="map_station_sec">
+                <div class="map_station_region" id="<?=esc_html($data['region-slug'])?>">
+                    <a href="#<?=esc_html($data['region-slug'])?>" class="map_station_region_title">
+                        <?php echo esc_html($data['region']) ?>
+                    </a>
+                </div>
+                <?php if (count($data['stations'])) {
+                    foreach ($data['stations'] as $k => $station) {                        
+                    ?>                
+                    <div class="map_station_sec" 
+                    data-title="<?= esc_html(isset($station['title']) ? $station['title'] : "") ?>"
+                    data-lat="<?=$station['location']['lat']?>"  
+                    data-lng="<?=$station['location']['lng']?>" 
+                    data-telephone="<?=esc_html(isset($station['telephone']) ? $station['telephone'] : "")?>" 
+                    data-information="<?= esc_html(isset($station['information']) ? $station['information'] : "") ?>"
+                    data-homepage="<?= esc_url(isset($station['homepage']) ? preg_replace('#^https?://#i', '', $station['homepage']) : "") ?>"
+                    
+                    >
                         <div class="map_station-items">
-                            <div>
-                                <a class="ms_button ms_button_view" title="<?= __("View in Map", 'station-map') ?>" target="_blank" href="https://maps.google.de/maps?q=<?= $station['location']['lat'] ?>, <?= $station['location']['lng'] ?>">
+                            <div class="map_station_mini_map" id="map_mini_<?=$station['id']?>" >
+
+                                <!-- <a class="ms_button ms_button_view" title="<?= __("View in Map", 'station-map') ?>" target="_blank" href="https://maps.google.de/maps?q=<?= $station['location']['lat'] ?>, <?= $station['location']['lng'] ?>">
                                     <i class="fa fa-map fa-3x"></i>
-                                </a>
+                                </a> -->
                             </div>
                         </div>
                         <div class="map_station-items map_station_infos">
                             <div class="map_station_infos_items">
-                                <div>
-
+                                <div class="map_station_infos_name" >
                                     <?= esc_html(isset($station['information']) ? $station['information'] : "") ?>
                                 </div>
                                 <div>
                                     <?= esc_html(isset($station['location']) ? $station['location']['address'] : "") ?>
                                 </div>
                                 <div>
-                                    <a href="<?= esc_url(isset($station['homepage']) ? $station['homepage'] : "") ?>"><?= esc_url(isset($station['homepage']) ? preg_replace('#^https?://#i', '', $station['homepage']) : "") ?></a>
+                                    <a href="<?= esc_url(isset($station['homepage']) ? $station['homepage'] : "") ?>"><?= (isset($station['homepage']) ? preg_replace('(^https?://)', '', $station['homepage']) : "") ?></a>
                                 </div>
                             </div>
                             <div class="map_station_infos_items">
@@ -89,6 +145,9 @@ if (post_type_exists('station')) {
                             </div>
                         </div>
                     </div>
+                <?php
+                    }
+                } ?>
 
             <?php
                 }
